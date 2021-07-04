534884301fc6306dff9d1032125bbf49ec1bfe01
juhaodong <juhaodong@gmail.com>
2021-07-04 22:37:09 +0200

    初始提交

---
+++ src/api/api.js
@@ -0,0 +1,60 @@
+export const selections = [
+  {
+    id: 'B', name: 'Primary front bodice neckline', subOptions: [
+      {id: 1, name: 'Straight'},
+      {id: 2, name: 'Sweetheart'},
+      {id: 3, name: 'V-neck'},
+      {id: 4, name: 'Basket'},
+      {id: 5, name: 'Scoop neck'},
+      {id: 6, name: 'Crewneck'},
+      {id: 7, name: 'Boatneck'},
+      {id: 8, name: 'Mockneck'},
+      {id: 9, name: 'Off the shoulder'},
+      {id: 10, name: 'Slant shoulder'},
+      {id: 11, name: 'Waterdrop'}
+    ]
+  },
+  {
+    id: 'C', name: 'Font bodices lining', subOptions: [
+      {id: '2a', name: 'Standard'},
+      {id: '2b', name: 'Subtle'},
+      {id: '2c', name: 'Angeled'},
+      {id: '2d', name: 'Pointed'},
+      {id: '2e', name: 'Narrow'}
+    ]
+  },
+  {id: 'D', name: 'Front bodices lining', subOptions: [
+      {id: 'Da', name: 'Unlined'},
+      {id: 'Db', name: 'Lined'},
+      {id: 'Dc', name: 'Totally unlined wich lace'}
+    ]},
+  {id: 'E', name: 'Bodice lace', subOptions: [
+      {id: 'Ea', name: 'Unlined'},
+      {id: 'Eb', name: 'Unlined'},
+      {id: 'Ec', name: 'Unlined'},
+      {id: 'Ed', name: 'Unlined'},
+      {id: 'Ee', name: 'Unlined'},
+      {id: 'Ef', name: 'Unlined'},
+    ]},
+  {id: 'F', name: 'Front bodice details', subOptions: [
+      {id: 'Fa', name: 'No details'},
+      {id: 'Fb', name: 'Folded Neckline'},
+      {id: 'Fc', name: 'Bodice ruching'},
+    ]},
+  {id: 'G', name: 'Body sleeves', subOptions: [
+      {id: 'Ga', name: 'No'},
+      {id: 'Gb', name: 'Short sleeve'},
+      {id: 'Gc', name: 'Middle lang sleeves'},
+      {id: 'Gd', name: 'Lang sleeves'},
+    ]},
+  {id: 'H', name: 'Straps details', subOptions: [
+      {id: 'Ha', name: 'Straight'},
+      {id: 'Hb', name: 'Flower'},
+      {id: 'Hc', name: 'Flared sleeves'},
+    ]},
+  {id: 'I', name: 'Sleeves detail: material', subOptions: [
+      {id: 'Ia', name: 'Fabric'},
+      {id: 'Ib', name: 'Nude tull'},
+      {id: 'Ic', name: 'Lace'}
+    ]}
+]
---
+++ src/views/Home.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="d-flex">
+    <v-sheet height="700px" style="width: 50%;">
+      <v-img width="100%" height="100%" :src="require('@/assets/girls-6174061_1920.jpg')"></v-img>
+    </v-sheet>
+    <v-card style="width: 50%">
+      <v-toolbar dense>
+        <v-tabs fixed-tabs v-model="currentTab">
+          <v-tab>Bo dice and sleeve</v-tab>
+          <v-tab>Skirt</v-tab>
+          <v-tab>Accessories</v-tab>
+        </v-tabs>
+      </v-toolbar>
+      <v-tabs-items v-model="currentTab">
+        <v-tab-item>
+          <v-expansion-panels accordion>
+            <v-expansion-panel
+                v-for="(item) in selections"
+                :key="item.id"
+            >
+              <v-expansion-panel-header>{{item.id}}.{{ item.name }}</v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-item-group>
+                  <div class="d-flex flex-wrap">
+                    <template v-for="option in item.subOptions">
+                      <v-item :key="option.id" v-slot="{active,toggle}">
+                        <div class="pa-2 weddingItem" @click="toggle" :class="active?' active':''"
+                             style="">
+                          <img style="height: 156px"
+                               :src="require('@/assets/image/ui/'+item.id+'/'+option.id+'.png')" alt=""/>
+                          <div class="option-label">{{ option.name }}</div>
+                        </div>
+                      </v-item>
+                    </template>
+                  </div>
+                </v-item-group>
+
+
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
+
+  </div>
+</template>
+
+<script>
+import { selections } from '@/api/api'
+
+export const defaultLook = {}
+
+export default {
+  name: 'Home',
+  data: function () {
+    return {
+      currentTab: 0,
+      selections: selections
+    }
+  },
+  components: {}
+}
+</script>
+<style>
+.option-label {
+  font-family: Palanquin;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 10px;
+  line-height: 18px;
+  text-align: center;
+  width: fit-content;
+
+  color: #4C4C4C;
+}
+
+.weddingItem{
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: center;
+}
+
+.weddingItem:hover {
+  border: 1px solid black;
+}
+
+.weddingItem.active .option-label{
+  border-bottom: 1px solid black;
+}
+</style>
