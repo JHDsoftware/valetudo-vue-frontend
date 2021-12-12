3c8b621e49c89fcd36054b8220a0854adc85f98f
IcyDresden <icydresden@gmail.com>
2021-12-12 21:37:50 +0100

    Update FAQ.vue

--- src/views/LoginAndRegister/FAQ.vue
+++ src/views/LoginAndRegister/FAQ.vue
@@ -1,8 +1,25 @@
 <template>
-<div class="d-flex flex-wrap justify-center">
-  <div>FAQ</div>
+<div class="d-flex flex-wrap justify-center align-center">
+  <div style="margin-top: 129px">FAQ</div>
   <div>
-    <v-text-field label="xxxxxx"></v-text-field>
+    <div>Konto</div>
+    <div><v-text-field></v-text-field></div>
+    <div><v-text-field></v-text-field></div>
+<!--    <v-text-field label="xxxxxx"></v-text-field>-->
+  </div>
+  <div>
+    <div>Zahlung</div>
+    <div><v-text-field /></div>
+  </div>
+
+  <div>
+    <div>Bestellung</div>
+    <v-text-field><v-text-field></v-text-field></v-text-field>
+  </div>
+
+  <div>
+    <div>Erstatting</div>
+    <v-for></v-for>
   </div>
 </div>
 </template>
