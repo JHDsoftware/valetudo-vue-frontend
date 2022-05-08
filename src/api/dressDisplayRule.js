787719308b342ee927c026ef6df581b929583a60
Haodong Ju <juhaodong@gmail.com>
2022-05-08 19:17:38 +0200

    修正了袖子的显示

--- src/api/dressDisplayRule.js
+++ src/api/dressDisplayRule.js
@@ -1,10 +1,10 @@
 export const views = ["Front", "Back"]
 export const excludeImage = ["Fa", "Fc", "Dc"]
-export const topSelection = ["B", "C", "D", "E", "F","G"]
+export const topSelection = ["B", "C", "D", "E", "F", "G"]
 export const skirtSelection = ["B", "C", "D", "E", "F", "G", "H", "I", "J"]
 export const sleevesSelection = ["G", "H", "I"]
 export const stripSelection = ["J"]
-export const backSelection = ["K", "L"]
+export const backSelection = ["J", "K", "L"]
 export const defaultTop = "Bodice-Front-B1-Da-Ea"
 export const defaultTopBack = "Bodice-Back-Ka-La"
 export const defaultSkirt = "Skirt-Front-B1-Da-Ga-Ha-Ia"
